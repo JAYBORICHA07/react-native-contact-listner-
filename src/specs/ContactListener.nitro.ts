cfd5cd4834483d2211fd54b831842493784ea66d
JAYBORICHA07 <jayboricha707@gmail.com>
2025-03-07 00:05:07 +0530

    created base interface for contact event and contact listener

--- src/specs/ContactListener.nitro.ts
+++ src/specs/ContactListener.nitro.ts
@@ -1,1 +1,14 @@
 // TODO: Export specs that extend HybridObject<...> here
+import type { HybridObject } from 'react-native-nitro-modules'
+
+export interface ContactListener
+  extends HybridObject<{ ios: 'swift'; android: 'kotlin' }> {
+  startListening(callback: (event: ContactEvent) => void): void
+
+  stopListening(): void
+}
+
+export interface ContactEvent {
+  type: 'added' | 'updated' | 'deleted'
+  contactId: string
+}
